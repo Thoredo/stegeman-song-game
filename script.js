51dc5fefb6b062f93dbb6f1c1c344c06fc091960
Jurgen-Stegeman <87040815+Thoredo@users.noreply.github.com>
2026-07-04 18:15:44 +0200

    add first script version

---
+++ script.js
@@ -0,0 +1,17 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+fetch("songs.json")
+    .then(res => res.json())
+    .then(data => {
+
+        const song = data.find(s => s.id == id);
+
+        if (!song) {
+            document.getElementById("title").innerText = "Song not found";
+            return;
+        }
+
+        document.getElementById("title").innerText =
+            `${song.Artist} - ${song.Title}`;
+    });
